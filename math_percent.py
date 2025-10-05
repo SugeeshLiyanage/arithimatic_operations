b1dc24c9525f3cbde650312d787079bec1fec033
Sugeesh Liyanage <sugeeshdeveloper@gmail.com>
2025-10-05 15:18:37 -0500

    Add files via upload

---
+++ math_percent.py
@@ -0,0 +1,7 @@
+math = int(input("enter youre math grade: "))
+english = int(input("enter youre english grade: "))
+sience = int(input("enter youre sience grade: "))
+hindi = int(input("enter youre hindi grade: "))
+sum = math + english + sience + hindi
+percentage = (sum / 400) * 100
+print(percentage)
